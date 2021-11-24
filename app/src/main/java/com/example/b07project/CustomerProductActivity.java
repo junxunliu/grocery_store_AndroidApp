8566c5096cf5615790399e9c22c29a45962186e1
ZionYin <ziyao.yin@mail.utoronto.ca>
2021-11-24 12:34:10 -0500

    Create Customer Product page

--- app/src/main/java/com/example/b07project/CustomerProductActivity.java
+++ app/src/main/java/com/example/b07project/CustomerProductActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CustomerActivity extends AppCompatActivity {
+public class CustomerProductActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
