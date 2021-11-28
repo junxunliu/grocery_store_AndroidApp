d5a2c48f7e80322f4335c93ad1874ac495cdfb14
ZionYin <ziyao.yin@mail.utoronto.ca>
2021-11-27 22:52:50 -0500

    implement StoreOwner

--- app/src/main/java/com/example/b07project/StoreOwner.java
+++ app/src/main/java/com/example/b07project/StoreOwner.java
@@ -1,4 +1,54 @@
 package com.example.b07project;
 
+import java.util.Objects;
+
 public class StoreOwner {
+    private String storeName;
+    private String address;
+    private ProductList productList = new ProductList(this);
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public ProductList getProductList() {
+        return productList;
+    }
+
+    public StoreOwner(){}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof StoreOwner)) return false;
+        StoreOwner that = (StoreOwner) o;
+        return getStoreName().equals(that.getStoreName()) && getAddress().equals(that.getAddress());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getStoreName(), getAddress());
+    }
+
+    public StoreOwner(String storeName, String address){
+        this.storeName=storeName;
+        this.address=address;
+    }
+
+    @Override
+    public String toString() {
+        return address + ":" + storeName;
+    }
 }
