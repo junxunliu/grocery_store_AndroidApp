4e26decf33bf8dc0cb1016b65af8cfd049a59ce4
cb07 <blairchen123@icloud.com>
2021-11-28 22:51:24 +0800

    Newest version

---
+++ app/src/main/java/com/example/b07project/CustomerStoreListViewActivity.java
@@ -0,0 +1,89 @@
+package com.example.b07project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
+public class CustomerStoreListViewActivity extends AddProductActivity implements  {
+    private ListView listStores;
+
+    private List<String> storeNames;
+    private String currentUserID;
+    private User currentUser;
+    private DatabaseReference usersRef;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_store_list);
+
+        currentUserID = getIntent().getStringExtra("currentUserID");
+
+        getCurrentUser();
+        getStoreNames();
+
+        listStores = (ListView) findViewById(R.id.listStores);
+        listStores.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String storeName = storeNames.get(i);
+                Intent intent = new Intent(CustomerStoreListViewActivity.this, DisplayStoreActivity.class);
+                intent.putExtra("storeName", storeName);
+                intent.putExtra("currentUser", (Parcelable) currentUser);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void getCurrentUser() {
+        FirebaseDatabase.getInstance().getReference("XXX")
+                .child("key").setValue(currentUser)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        User user = snapshot.getValue(User.class);
+                        callback.accept(user);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                });
+    }
+
+
+
+    private void getStoreNames() {
+
+    }
+
+
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_order:
+                startActivity(new Intent(this, CustomerOrderListActivity.class));
+                break;
+        }
+    }
+}
