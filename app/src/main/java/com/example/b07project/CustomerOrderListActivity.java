8300cd0f3f767ad34b1579b17f188531afc645a5
Xinyi-Ye <xinyi.ye@mail.utoronto.ca>
2021-11-29 17:17:35 +0800

    construct CustomerOrderListActivity, modify OrderList Class, construct layout for activity_store_owner_order_list and store_order

--- app/src/main/java/com/example/b07project/CustomerOrderListActivity.java
+++ app/src/main/java/com/example/b07project/CustomerOrderListActivity.java
@@ -2,13 +2,47 @@ package com.example.b07project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CustomerOrderListActivity extends AppCompatActivity {
+
+    private TextView tv_display;
+    private Button btn_storeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_order_list);
+
+        init();
+        display();
+
+        btn_storeList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(CustomerOrderListActivity.this, target_activity.class);
+                //startActivity(intent);
+            }
+        });
+    }
+
+    private void init(){
+        tv_display.findViewById(R.id.textView2);
+        btn_storeList.findViewById(R.id.button2);
+    }
+
+    private void display(){
+        OrderList orderlist = new OrderList();
+        orderlist.readFromDB();
+        OrderList customerOrderList = new OrderList();
+        //User implements Serializable
+        //data from customer main page: getIntent().getSerializableExtra();
+        //Person person =(Person) getIntent().getSerializableExtra("person_data");
+        //customerOrderList = orderlist.search(user);
+        tv_display.setText(customerOrderList.toString());
     }
 }
