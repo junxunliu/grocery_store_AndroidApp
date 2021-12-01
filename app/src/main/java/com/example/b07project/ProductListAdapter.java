7f2677edd3c47b8b905fbae539a2484bf833ff7b
Dlontheway <devinfromc@gmail.com>
2021-12-01 22:52:31 +0800

    update addProduct

--- app/src/main/java/com/example/b07project/ProductListAdapter.java
+++ app/src/main/java/com/example/b07project/ProductListAdapter.java
@@ -16,6 +16,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProductListAdapter extends ArrayAdapter<Product> {
